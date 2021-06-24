927b5f764875757973adcb35b8badc92e560a0ea
taha <taha7900@gmail.com>
2021-06-24 21:14:59 +0430

    fresh android project

---
+++ app/src/main/java/ir/taha7900/yadnegar/MainActivity.java
@@ -0,0 +1,14 @@
+package ir.taha7900.yadnegar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
