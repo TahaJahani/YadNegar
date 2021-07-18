19b021ce950c22eef7842899f8ddd24a64d64f17
taha <taha7900@gmail.com>
2021-07-18 16:49:34 +0430

    Adapter for memory added

---
+++ app/src/main/java/ir/taha7900/yadnegar/Adapters/MemoryAdapter.java
@@ -0,0 +1,33 @@
+package ir.taha7900.yadnegar.Adapters;
+
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class MemoryAdapter extends RecyclerView.Adapter<MemoryAdapter.ViewHolder> {
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    static class ViewHolder extends RecyclerView.ViewHolder {
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+}
