67113768b069a877bd9337cdb354b9e7432c7542
taha <taha7900@gmail.com>
2021-07-25 16:30:35 +0430

    file preview added

--- app/src/main/java/ir/taha7900/yadnegar/WebViewFragment.java
+++ app/src/main/java/ir/taha7900/yadnegar/WebViewFragment.java
@@ -1,18 +1,26 @@
 package ir.taha7900.yadnegar;
 
+import android.app.ProgressDialog;
+import android.content.Context;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class WebViewFragment extends Fragment {
 
     private static final String ARG_ADDRESS = "address";
 
     private String address;
+    private WebView webView;
+    private ProgressDialog progressDialog;
+    private MainActivity context;
 
     public WebViewFragment() {
         // Required empty public constructor
@@ -35,9 +43,41 @@ public class WebViewFragment extends Fragment {
     }
 
     @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        this.context = (MainActivity) context;
+    }
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_web_view, container, false);
+        View view = inflater.inflate(R.layout.fragment_web_view, container, false);
+        webView = view.findViewById(R.id.webView);
+        System.out.println(address);
+
+        progressDialog = ProgressDialog.show(context, "Loading", "Please wait...", true);
+        progressDialog.setCancelable(false);
+
+
+        webView.getSettings().setLoadWithOverviewMode(true);
+        webView.getSettings().setUseWideViewPort(true);
+        webView.setWebViewClient(new WebViewClient() {
+
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                progressDialog.show();
+                view.loadUrl(url);
+
+                return true;
+            }
+
+            @Override
+            public void onPageFinished(WebView view, final String url) {
+                progressDialog.dismiss();
+            }
+        });
+
+        webView.loadUrl(address);
+        return view;
     }
 }
